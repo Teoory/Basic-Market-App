6c4e854ebcb1d0f1aa9b089165b1999a712b7812
Berkay Köksal <berkay.koksall54@gmail.com>
2025-02-23 22:11:58 +0300

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,15 +2,15 @@ const Footer = () => {
     return (
         <footer className="footer">
                 <p>
-                    Bu site VORP sunucusu için geliştirilmiştir. {' '}
+                    Bu site GTA 5 modu, Fivem de kullanılmak için geliştirilmiştir. {' '}
                 </p>
                 <a href="/usage-and-purpose" target="_blank" rel="noopener noreferrer">Kullanım ve Amaç</a>
                 <div className="footer-links">
-                    <a href="https://discord.gg/YJQHY7w4" target="_blank" rel="noopener noreferrer">
+                    <a href="https://discord.gg/" target="_blank" rel="noopener noreferrer">
                         Discord
                     </a>
-                    <a href="https://Vorp.gg/" target="_blank" rel="noopener noreferrer">
-                        Vorp.gg
+                    <a href="https://kofu.com.tr/" target="_blank" rel="noopener noreferrer">
+                        kofur.com.tr
                     </a>
                 </div>
         </footer>
